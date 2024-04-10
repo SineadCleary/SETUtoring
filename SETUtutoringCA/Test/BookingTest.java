3419417c2a540fb7b05de1a7071b5ec0361dbefe
EmmaC1804 <20102506@mail.wit.ie>
2024-04-10 15:00:19 +0100

    Booking tests started

--- SETUtutoringCA/Test/BookingTest.java
+++ SETUtutoringCA/Test/BookingTest.java
@@ -8,6 +8,13 @@ public class BookingTest {
 
     @Before
     void setup(){
+        group1 = new Group(12);
+        group2 = new Group(7);
+        group3 = new Group(3);
 
+        room1 = new Room(0,8);
+        room2 = new Room(2,3);
+        room3 = new Room(3,9);
     }
+
 }
