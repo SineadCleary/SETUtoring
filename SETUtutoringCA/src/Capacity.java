5c4cb7057ae8fa5efac29db56259b1bc41fd889d
SineadCleary <20102299@mail.wit.ie>
2024-04-10 14:58:39 +0100

    Capacity

--- SETUtutoringCA/src/Capacity.java
+++ SETUtutoringCA/src/Capacity.java
@@ -1,4 +1,9 @@
 public class Capacity {
+    public String verifyCapacity(Room room, Group group){
 
-
+        if(room.getCapacity() <= group.getMembers()) {
+            return "Class created";
+        }
+        else return "Class too big for room size";
+    }
 }
